e2520242b20d959f09d10157d0f43130e8a9c75c
Salvino Modica <salvymody@gmail.com>
2023-07-14 06:26:02 +0000

    feat: add turn cycle and compare odd-even logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,67 @@
 /* creating the sections that will be interchangable */
-
 const menu = document.getElementById("menu");
 const rulesSection = document.getElementById("rules-section");
 const gameSection = document.getElementById("game-section");
 const oddEvenSection = document.getElementById("odd-even-container");
 const wagerSection = document.getElementById("wager-container");
-const playerMarbles = document.getElementById("player-marble");
-const computerMarbles = document.getElementById("computer-marble");
 
+// Set variables to various buttons
+
+const evenButton = document.getElementById("even-button");
+const oddButton = document.getElementById("odd-button");
+const quitButton = document.getElementById("quit-button");
+const startButton = document.getElementById("start-button");
+const closeRulesButton = document.getElementById("rules-close");
+const openRulesButton = document.getElementById("rules-open");
+
+// Score
+let playerMarblesCounter = document.getElementById("player-marble");
+let computerMarblesCounter = document.getElementById("computer-marble");
+
+// Status Message
 let playerStatus = document.getElementById("player-status");
 
+// Game stats
 let playerTurn = true;
+let playerOE;
+let playerWager;
+let playerMarbles;
+let computerMarbles;
 
 /* Event listeners */
-
-let openRulesButton = document.getElementById("rules-open");
 openRulesButton.addEventListener("click", openRules);
 
-let closeRulesButton = document.getElementById("rules-close");
 closeRulesButton.addEventListener("click", closeRules);
 
-let startButton = document.getElementById("start-button");
 startButton.addEventListener("click", startGame);
 
-let quitButton = document.getElementById("quit-button");
 quitButton.addEventListener("click", quitGame);
 
-let oddButton = document.getElementById("odd-button");
-oddButton.addEventListener("click", chooseOdd);
-
-let evenButton = document.getElementById("even-button");
-evenButton.addEventListener("click", chooseEven);
-
+// Event listener to give value of wager to a variable and call a function 
 let wagerButtons = document.getElementsByClassName("wager-button");
     for (button of wagerButtons) {
-        button.addEventListener("click", chooseWager);
+        button.addEventListener("click", function() {
+            wager = parseInt(this.value);
+            handleWager(wager);
+        });
     }
 
+// Event listener for OE buttons to call a function
+oddButton.addEventListener("click", function() {
+        handleOE("odd");
+    });
+
+evenButton.addEventListener("click", function() {
+        handleOE("even");
+    });
+
 /* Open and close rules section by toggling display:none to display:flex */
-
 function openRules(event) {
-
     rulesSection.style.display = "flex";
 }
 
 function closeRules(event) {
-
     rulesSection.style.display = "none";
-}
-
-/* Start Button removes Menu and toggles Game screen */
-
-function startGame(event) {
-
-    menu.style.display = "none";
-    gameSection.style.display = "flex";
-
-    playerMarbles.textContent = 10;
-    computerMarbles.textContent = 10;
-
-    playGame();
 }
 
 /* Quit button returns to the Menu */
@@ -76,18 +78,60 @@ function switchTurn () {
     playerTurn = !playerTurn;
 }
 
-// Updates marbles counter
-function updateMarblesDisplay() {
-    playerMarbles.textContent = playerMarbles;
-    computerMarbles.textContent = computerMarbles;
+// Function to handle the player's choice on it's turn
+function handleOE(oe) {
+    playerOE = oe;
+    hideOE();
+    showWager();
 }
+
+function handleWager (wager) {
+    playerWager = wager;
+    hideWager();
+    compareOE();
+}
+
+// Function to roll computer OE, compare with the player and repeat the game cycle
+
+function compareOE() {
+
+    let randomNumber = Math.floor(Math.random*4) + 1;
+    computerOE = randomNumber === 1 || randomNumber === 3 ? "odd" : "even";
+
+    let playerWins = playerOE === computerOE;
+
+    if (playerWins) {
+
+        playerMarbles += playerWager;
+        computerMarbles -= playerWager;
+        playerStatus.innerHTML = "<p>You <span class='hider'>Win!</span></p>";
+
+    } else {
+
+        playerMarbles -= playerWager;
+        computerMarbles += playerWager;
+        playerStatus.innerHTML = "<p>You <span class='guesser'>Lost!</span></p>";
+
+    }
+
+    playerMarblesCounter.textContent = playerMarbles;
+    computerMarblesCounter.textContent = computerMarbles;
+
+    setTimeout(function() {
+        
+        switchTurn();
+        if (playerTurn) {
+          showOE();
+        } else {
+          showOE();
+        }
+      }, 3000);
+    }
+
+// Functions to either show or hide OE and wager buttons
 
 function showOE() {
     oddEvenSection.style.display = "flex";
-}
-
-function chooseOE() {
-
 }
 
 function hideOE() {
@@ -98,34 +142,34 @@ function showWager() {
     wagerSection.style.display = "flex";
 }
 
-function chooseWager (event) {
-    let selectedWager = parseInt(this.getAttribute("data-value"));
-}
-
 function hideWager() {
     wagerSection.style.display = "none";
 }
 
+// Function to tell the player what turn it is to guess or hide
 function playerGuesser() {
-    playerStatus.innerHTML = "<p>You are the <span class='guesser'>Guesser!</span>!</p>";
+    playerStatus.innerHTML = "<p>You are the <span class='guesser'>Guesser!</span></p>";
 }
 
-function playGame() {
+/* Start Button removes Menu and toggles Game screen */
 
+function startGame(event) {
+
+    menu.style.display = "none";
+    gameSection.style.display = "flex";
+
+    playerMarbles = 10;
+    computerMarbles = 10;
+    
+    playerMarblesCounter.textContent = playerMarbles;
+    computerMarblesCounter.textContent = computerMarbles;
+    
+    playgame();
+}
+
+function playgame() {
     if (playerTurn) {
         playerGuesser();
         showOE();
-        chooseOE();
-        hideOE();
-    } else {
-        playerHider();
-    }
-
-    if (parseInt(playerMarbles.textContent) > 0 && parseInt(playerMarbles.textContent) < 20) {
-        playGame();
-    } else if (parseInt(playerMarbles.textContent) == 20) {
-        victory();
-    } else if (parseInt(playerMarbles.textContent) == 0) {
-        defeat();
     }
 }
